730a514b5e4328917b51c22fa8884c203d1c6aea
Javier Mas Adell <javier.mas@barcelonagse.eu>
2018-01-20 18:52:32 +0100

    :sparkles: :zap: created script to validate de LSTM

---
+++ airquality/data/read_data.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from airquality.data.prepare_data import create_ts_df
+
+
+def read_obs(path='/Users/b.yc0006/Cloud/BCNAirQualityDatathon/data/processed/all_obs.csv'):
+    data = pd.read_csv(path).rename(columns={
+    'AirQualityStationEoICode':'station',
+    'DatetimeBegin': 'datetime',
+    'Concentration': 'concentration'
+})
+    data['datetime'] = pd.to_datetime(data['datetime'])
+    data = data.sort_values(['station', 'datetime']).reset_index(drop=True)
+    data = create_ts_df(data, 'datetime')
+    data['date'] = data['datetime'].dt.date
+    return data
+
+
+def read_targets(path='/Users/b.yc0006/Cloud/BCNAirQualityDatathon/data/raw/targets.csv'):
+    target = pd.read_csv(path).rename(columns={
+        'target':'concentration',
+    })
+    target['date'] = pd.to_datetime(target['date'])
+    target = target.sort_values(['station', 'date']).reset_index(drop=True)
+    target = create_ts_df(target, 'date')
+    return target
---
+++ airquality/models/validate_lstm.py
@@ -0,0 +1,31 @@
+from airquality.data.read_data import read_obs, read_targets
+from airquality.models.LSTM_keras import LSTM_K
+from airquality.data.prepare_data import create_model_matrix
+from airquality.models.split import tt_split, reshape_to_keras
+
+# Read data
+data_obs = read_obs()
+data_targets = read_targets()
+target_cols = data_targets.columns[:-1]
+
+# Prepare data
+seq_length = 1
+model_matrix = create_model_matrix(data_obs, target_cols=list(target_cols))
+train_size = int(len(model_matrix)*0.7)
+train_X, test_X, train_Y, test_Y = tt_split(model_matrix, train_size, target_cols)
+train_X = reshape_to_keras(train_X, seq_length)
+test_X = reshape_to_keras(test_X, seq_length)
+
+# Validate LSTM
+param_dict = {
+    'batch_size': 10,
+    'seq_length': seq_length,
+    'size': train_X.shape[2],
+    'hidden_units': 50,
+    'num_layers': 1,
+    'dense_units': train_Y.shape[1],
+    'epochs': 50,
+}
+
+lstm = LSTM_K(**param_dict)
+lstm.validate(train_X, train_Y, test_X, test_Y)
