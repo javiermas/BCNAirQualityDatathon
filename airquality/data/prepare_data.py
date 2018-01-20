5b3f7591fb4e2c324f54b96483a262bf485c71eb
Javier Mas Adell <javier.mas@barcelonagse.eu>
2018-01-20 11:05:55 +0100

    :sparkles: Started notebooks and LSTM_keras

--- airquality/data/prepare_data.py
+++ airquality/data/prepare_data.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
 
-def create_model_matrix(features, target, lags):
+def create_model_matrix(features=None, target=None, lags=1):
+    '''Pass features and target as dataframes.'''
+    if features is None:
+        lagged_target = create_lagged_features(target, lags)
+        model_matrix = pd.concat([target, lagged_target], axis=1).dropna()
+        return model_matrix
+
     features = create_lagged_features(pd.concat([features, target], axis=1), lags)
-    model_matrix = pd.concat([features, target], axis=1)
+    model_matrix = pd.concat([features, target], axis=1).dropna()
     return model_matrix
 
 
@@ -11,15 +17,15 @@ def create_lagged_features(data, lags):
     '''This function takes a time series and
     returns a dataframe with <lags> 
     1-to-lag lagged features.'''
-    if not isinstance(data, pd.DataFrame):
-        data = pd.DataFrame(data)
-    
     data_list = []
-    for lag in range(lags+1):
+    for lag in range(1, lags+1):
         shifted_data = data.shift(lag)
-        colnames = [col+'_lag_'+str(lag) for col in data.columns]
-        shifted_data.columns = colnames
+        col_dict = {}
+        for col in data.columns:
+            col_dict[col] = col+'_lag_'+str(lag)
+
+        shifted_data = shifted_data.rename(columns=col_dict)
         data_list.append(shifted_data)
     
-    data = pd.concat(data_list, axis=1).dropna()
+    data = pd.concat(data_list, axis=1)
     return data
